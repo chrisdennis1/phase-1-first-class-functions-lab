f4a2f33d6fdc47ca359961e552c60f2fe787faa0
chrisdennis12345@gmail.com <chrisdennis@Chriss-iMac.local>
2022-08-09 10:48:44 -0700

    commit tree 984da0d16bb17e6b926f9df6432881c5bcfa1fea

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+  };
+
+  const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+
+  const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+  function createFareMultiplier(num){
+      return function (number){
+         return number * num
+      }
+  }
+
+  function fareTripler(num){
+    return num * 3
+  }
+
+  function fareDoubler(num){
+    return num * 2
+  }
+
+  const selectDifferentDrivers = function (drivers, whichDrivers) {
+    return whichDrivers(drivers);
+  }
+  
